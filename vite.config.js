84c5052f3c981f6256de76676ddd5fed06342dc5
Karlito14 <leiroz26@hotmail.com>
2024-06-08 08:42:21 +0200

    mise en place du projet

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
+import { build } from 'vite';
+import { resolve } from 'path';
+
 export default {
   server: {
     port: 3000,
+  },
+  root: 'src',
+  build: {
+    outdir: '../dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        form: resolve(__dirname, 'src/form/form.html'),
+      },
+    },
   },
 };
