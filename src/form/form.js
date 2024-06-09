8d84a61841b325b5ea02a85d5ce2ce9396a52145
Karlito14 <leiroz26@hotmail.com>
2024-06-09 21:37:46 +0200

    gestion erreurs formulaire

--- src/form/form.js
+++ src/form/form.js
@@ -2,3 +2,47 @@ import '../assets/styles/global.scss';
 import '../assets/styles/style.scss';
 import './form.scss';
 
+const form = document.querySelector('#form');
+const elDeleteBtn = form.querySelector('#delete_btn');
+const divError = form.querySelector('#div-error');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  let messageError = document.querySelector('#message-error');
+
+  const object = {};
+
+  for (const entry of formData) {
+    let value = entry[1].trim();
+    value = value.replaceAll('  ', ' ');
+    value = value[0]?.toUpperCase() + value.slice(1);
+    object[entry[0]] = value;
+  }
+
+  if (formIsValid(object)) {
+    const json = JSON.stringify(object);
+    if (messageError) {
+      messageError.remove();
+    }
+    console.log(json);
+  } else {
+    if (!messageError) {
+      messageError = document.createElement('p');
+      messageError.setAttribute('class', 'text-error');
+      messageError.setAttribute('id', 'message-error');
+      messageError.textContent =
+        'Veuillez remplir toutes les champs obligatoires';
+      divError.append(messageError);
+    }
+  }
+});
+
+const formIsValid = (object) => {
+  for (const property in object) {
+    if (object[property] === 'undefined') {
+      return false;
+    }
+  }
+  return true;
+};
