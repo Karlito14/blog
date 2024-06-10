d5e970a0f4a94e56b65e6a14f7b3d79f1271890a
Karlito14 <leiroz26@hotmail.com>
2024-06-10 14:34:28 +0200

    suppression article

--- src/api/api-articles.js
+++ src/api/api-articles.js
@@ -5,7 +5,7 @@ class ApiArticles {
 
   async getArticles() {
     try {
-      const promesse = await fetch(`${this.BASE_URL}/articles`);
+      const promesse = await fetch(`${this.BASE_URL}`);
       const resultat = await promesse.json();
       return resultat;
     } catch (error) {
@@ -15,7 +15,7 @@ class ApiArticles {
 
   async sendArticle(json) {
     try {
-      const promesse = await fetch(`${this.BASE_URL}/articles`, {
+      const promesse = await fetch(`${this.BASE_URL}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -30,6 +30,16 @@ class ApiArticles {
       console.error('Erreur :', erreur);
     }
   }
+
+  async deleteArticle(id) {
+    try {
+      const promesse = await fetch(`${this.BASE_URL}/${id}`, {
+        method: 'DELETE',
+      });
+    } catch (erreur) {
+      console.error('Erreur :', erreur);
+    }
+  }
 }
 
-export default new ApiArticles('https://restapi.fr/api');
+export default new ApiArticles('https://restapi.fr/api/items');
--- src/index.js
+++ src/index.js
@@ -5,13 +5,41 @@ console.log(articles);
 const elList = document.querySelector('.list');
 const template = document.querySelector('#template_article');
 
-articles.forEach((article) => {
+if (Array.isArray(articles)) {
+  displayArticles(articles);
+} else if (typeof articles === 'object') {
+  displaySingleArticle(articles);
+}
+
+function displayArticles(articles) {
+  elList.innerHTML = '';
+
+  articles.forEach((article) => {
+    const clone = createElement(article, template);
+    const btnDelete = clone.children[0].querySelector('#delete_btn');
+
+    btnDelete.addEventListener('click', async () => {
+      await apiArticles.deleteArticle(article._id);
+      const index = findIndex(articles, article._id);
+      articles.splice(index, 1);
+      displayArticles(articles);
+    });
+
+    elList.append(clone);
+  });
+}
+
+function displaySingleArticle(article) {
   const clone = createElement(article, template);
   const btnDelete = clone.children[0].querySelector('#delete_btn');
-  btnDelete.setAttribute('data-id', article._id);
-  console.log(btnDelete);
+
+  btnDelete.addEventListener('click', async () => {
+    await apiArticles.deleteArticle(article._id);
+    elList.innerHTML = '';
+  });
+
   elList.append(clone);
-});
+}
 
 function createElement(article, template) {
   const clone = template.content.cloneNode(true);
@@ -26,4 +54,8 @@ function createElement(article, template) {
   content.textContent = article.content;
 
   return clone;
+}
+
+function findIndex(articles, id) {
+  return articles.findIndex((article) => article._id === id);
 }
