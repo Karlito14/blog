4387a3d8cd7db2adab3318b69f2cb0d18610b7f4
Karlito14 <leiroz26@hotmail.com>
2024-06-12 17:02:04 +0200

    ajout animation categorie selectionné

--- src/index.js
+++ src/index.js
@@ -11,10 +11,8 @@ const optionsDate = {
   year: 'numeric',
 };
 
-console.log(articles);
-
 initPage(articles);
-createMenuCategory(articles);
+displayMenuCategory(articles);
 
 function initPage(articleList) {
   if (Array.isArray(articleList)) {
@@ -76,40 +74,47 @@ function createElement(article, template) {
   return clone;
 }
 
-function createMenuCategory(articleList) {
+function displayMenuCategory(articleList) {
   const categories = retrieveCategories(articleList);
 
   for (const category in categories) {
     const li = document.createElement('li');
     li.setAttribute('class', 'item-category');
+    li.setAttribute('data-id', category);
     li.textContent = `- ${category} (${categories[category]})`;
     ElCategoryList.append(li);
+  }
 
-    li.addEventListener('click', () => {
-      if (category === 'Tous') {
+  const elementsCategory = document.querySelectorAll('.item-category');
+
+  elementsCategory.forEach((category) => {
+    category.addEventListener('click', () => {
+      if (category.classList.contains('active')) {
+        category.classList.remove('active');
         initPage(articles);
       } else {
+        elementsCategory.forEach((element) =>
+          element.classList.remove('active')
+        );
+        category.classList.add('active');
         const filterArticles = articleList.filter(
-          (article) => article.category === category
+          (article) => article.category === category.dataset.id
         );
         initPage(filterArticles);
       }
     });
-  }
+  });
 }
 
 function retrieveCategories(articleList) {
-  const categories = articleList.reduce(
-    (acc, curr) => {
-      if (acc[curr.category]) {
-        acc[curr.category]++;
-      } else {
-        acc[curr.category] = 1;
-      }
-      return acc;
-    },
-    { Tous: articleList.length }
-  );
+  const categories = articleList.reduce((acc, curr) => {
+    if (acc[curr.category]) {
+      acc[curr.category]++;
+    } else {
+      acc[curr.category] = 1;
+    }
+    return acc;
+  }, {});
 
   return categories;
 }
